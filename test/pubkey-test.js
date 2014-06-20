1de1379adf7c0578adef95125193988e93636a8c
Oscar Jacobsson <oscar@ayoy.se>
2014-06-20 05:28:50 +0200

    Added ES384 and ES512 public key tests

--- test/pubkey-test.js
+++ test/pubkey-test.js
@@ -3,15 +3,23 @@ var expect = require('chai').expect;
 var pubKey = require('../lib/pubkey');
 
 describe('pubKey', function() {
-    var PAYLOAD = new Buffer('Hello, world!', 'ascii');
-    var ALICE_P256_PRIVATE_KEY;
-    var ALICE_P256_PUBLIC_KEY;
-    var ALICE_RSA_PRIVATE_KEY;
-    var ALICE_RSA_PUBLIC_KEY;
+    var PAYLOAD = new Buffer('Hello, world!', 'ascii'),
+        ALICE_P256_PRIVATE_KEY,
+        ALICE_P256_PUBLIC_KEY,
+        ALICE_P384_PRIVATE_KEY,
+        ALICE_P384_PUBLIC_KEY,
+        ALICE_P521_PRIVATE_KEY,
+        ALICE_P521_PUBLIC_KEY,
+        ALICE_RSA_PRIVATE_KEY,
+        ALICE_RSA_PUBLIC_KEY;
 
     before(function() {
         ALICE_P256_PRIVATE_KEY = fs.readFileSync(__dirname + '/../testdata/es256-alice-priv.pem').toString();
         ALICE_P256_PUBLIC_KEY  = fs.readFileSync(__dirname + '/../testdata/es256-alice-pub.pem').toString();
+        ALICE_P384_PRIVATE_KEY = fs.readFileSync(__dirname + '/../testdata/es384-alice-priv.pem').toString();
+        ALICE_P384_PUBLIC_KEY  = fs.readFileSync(__dirname + '/../testdata/es384-alice-pub.pem').toString();
+        ALICE_P521_PRIVATE_KEY = fs.readFileSync(__dirname + '/../testdata/es512-alice-priv.pem').toString();
+        ALICE_P521_PUBLIC_KEY  = fs.readFileSync(__dirname + '/../testdata/es512-alice-pub.pem').toString();
         ALICE_RSA_PRIVATE_KEY  = fs.readFileSync(__dirname + '/../testdata/rs256-alice-priv.pem').toString();
         ALICE_RSA_PUBLIC_KEY   = fs.readFileSync(__dirname + '/../testdata/rs256-alice-pub.pem').toString();
     });
@@ -21,6 +29,22 @@ describe('pubKey', function() {
             var signature = pubKey.doSign('sha256', ALICE_P256_PRIVATE_KEY, PAYLOAD);
 
             expect(pubKey.doVerify('sha256', ALICE_P256_PUBLIC_KEY, PAYLOAD, signature)).to.equal(true);
+        });
+    });
+
+    describe('ES384', function() {
+        it('should correctly sign and verify ES384 signatures', function() {
+            var signature = pubKey.doSign('sha384', ALICE_P384_PRIVATE_KEY, PAYLOAD);
+
+            expect(pubKey.doVerify('sha384', ALICE_P384_PUBLIC_KEY, PAYLOAD, signature)).to.equal(true);
+        });
+    });
+
+    describe('ES512', function() {
+        it('should correctly sign and verify ES512 signatures', function() {
+            var signature = pubKey.doSign('sha512', ALICE_P521_PRIVATE_KEY, PAYLOAD);
+
+            expect(pubKey.doVerify('sha512', ALICE_P521_PUBLIC_KEY, PAYLOAD, signature)).to.equal(true);
         });
     });
 
